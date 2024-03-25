33af7d12712fe46c1d0c928f81446a756e6a779d
adorahelen <adorahelenmin@gmail.com>
2024-03-25 23:13:15 +0900

    2024_03_25 delete code 작성(JS/HTML)..동작 안함;

---
+++ src/main/resources/static/article.js
@@ -0,0 +1,15 @@
+// 삭제 기능 구현
+const deleteButton = document.getElementById('delete-btn');
+
+if (deleteButton) {
+    deleteButton.addEventListener('click', event => {
+        let id = document.getElementById('article-id').value;
+        fetch(`/api/articles/${id}`, {
+            method: 'DELETE'
+        })
+            .then(() => {
+                alert('삭제가 완료되었습니다.');
+                location.replace('/articles');
+            });
+    });
+} // 자바 스클립트를 이용한 js 파일
