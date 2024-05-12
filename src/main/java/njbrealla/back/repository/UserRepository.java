aaf448a8652fe3af2d765aafb6fab8b68a6df15b
adorahelen <adorahelenmin@gmail.com>
2024-05-12 22:00:06 +0900

    2024_05-12 . JPA사용하는 레포지토리 작성

---
+++ src/main/java/njbrealla/back/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package njbrealla.back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+import java.util.Optional;
+
+public interface  UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
